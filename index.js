3006b1990380a28b706234a5aed572b335661d23
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-03-25 16:22:21 +0100

    funkce: maximum ze dvou cisel

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*ZADANI 1. */
 const ellipseArea = (width, height) => {
   return (width / 2) * (height / 2) * Math.PI
 }
@@ -7,3 +8,13 @@ console.log(ellipseArea(2, 2));
 
 console.log(Math.round(ellipseArea(3, 2)));
 
+/*ZADANI 2. */
+
+const max2 = (cislo1, cislo2) => {
+  if (cislo1 > cislo2) {
+    return "je vetsi"
+  }
+  return "je mensi"
+}
+
+console.log(max2(2, 5))
